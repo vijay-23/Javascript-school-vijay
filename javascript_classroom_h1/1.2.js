90f4b9b8eea984fa5c78081dc7da557fa3083daf
Vijay <vjhoelas@gmail.com>
2020-01-09 10:02:35 +0100

    javascript: classroom1 afgerond

---
+++ javascript_classroom_h1/1.2.js
@@ -0,0 +1,3 @@
+for( var i=0; i <= 9; i++) {
+    console.log("De waarde van i is: " + i);   
+}
---
+++ javascript_classroom_h1/1.3.js
@@ -0,0 +1,8 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
