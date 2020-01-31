5e6c194d605096af4541de7299032139d0a76389
Vijay <vjhoelas@gmail.com>
2020-01-31 21:58:10 +0100

    DOM opdacht af

---
+++ javascript_classroom_h2/2.1.js
@@ -0,0 +1,11 @@
+var paginaTitel = document.querySelector("#paginatitel");
+var subKop = document.querySelector("#subkop");
+
+var panther1 = document.querySelector("#panther1");
+var panther2 = document.querySelector("#panther2");
+
+paginaTitel.textContent = "Dit is een pagina titel";
+subKop.textContent = "Dit is een subkop";
+
+panther1.setAttribute("src", "../javascript_classroom_h3/img/blackpanther1.jpg");
+panther2.setAttribute("src", "../javascript_classroom_h3/img/blackpanther2.jpg");
---
+++ javascript_classroom_h2/2.2.js
@@ -0,0 +1,19 @@
+var blok1 = document.querySelector("#block1");
+var blok2 = document.querySelector("#block2");
+var blok3= document.querySelector("#block3");
+var blok4 = document.querySelector("#block4");
+var tekst1 = document.querySelector("#block1");
+
+
+blok1.style.backgroundColor = "#03fcca";
+blok2.style.backgroundColor = "#008507";
+blok3.style.backgroundColor = "#f7ff1c";
+blok4.style.backgroundColor = "#f20000";
+
+blok2.style.marginTop = "20px";
+blok3.style.marginTop = "45px";
+
+block1.style.textAlign = "center";
+block1.style.paddingTop = "20px";
+block2.style.textAlign = "center";
+block3.style.textAlign= "right";
